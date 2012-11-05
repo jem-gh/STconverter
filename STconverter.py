a75a1609ab66d7b635cb39dd87f9db46ddd37e7e
jem-gh <jem.github@gmail.com>
2012-11-05 17:10:17 +0100

    fix up_ini to only update the starting of the event loop and not all ".start()"

--- STconverter.py
+++ STconverter.py
@@ -236,7 +236,8 @@ class Simplegui2Tkinter:
     def up_ini(self):
         """ update the initialization of the event loop """
         global output_data
-        START_RE = re.compile(r'^\w+.start\(\)', re.MULTILINE)
+        frame_name = re.findall(r"(\w+) = Tkinter.Frame\(", output_data)
+        START_RE = re.compile(r"^%s.start\(\)" % frame_name[0], re.MULTILINE)
         output_data = START_RE.sub("window_root.mainloop()\n", output_data)
     
     
