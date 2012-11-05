86f45acdf2f04313efd6ba0c3efa8c30e37019c5
jem-gh <jem.github@gmail.com>
2012-11-05 14:53:38 +0100

    sg>tk conversion: accept variable for size value during frame creation

--- STconverter.py
+++ STconverter.py
@@ -67,7 +67,7 @@ class Simplegui2Tkinter:
         
         frame_widget = {
         "sg_frame":  "^(\w+) ?= ?simplegui.create_frame" + \
-                     "\((\".+\"), ?(\d+), ?(\d+)(?:, ?\d+)?\)", 
+                     "\((\".+\"), ?(\w+), ?(\w+)(?:, ?\d+)?\)", 
         "tk_frame":  "window_root = Tkinter.Tk()\n" + \
                      "window_root.title(\\2)\n" + \
                      "\\1 = Tkinter.Frame(window_root)\n" + \
