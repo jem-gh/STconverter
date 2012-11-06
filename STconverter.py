44e6f6653bdb434beee4cf8b6ecc1bc70e18a250
jem-gh <jem.github@gmail.com>
2012-11-06 16:26:18 +0100

    fix: sg>tk Button conversion followed by a comment "#..."

--- STconverter.py
+++ STconverter.py
@@ -161,8 +161,8 @@ class Simplegui2Tkinter:
         """ update Button widget(s) """
         global output_data
         button_widget = {
-        "sg_button": "(?:\w+ ?= ?)(\w+).add_button\((.+), ?(\w+), ?(\d+)\d\)", 
-        "tk_button": "\\3_bt = Tkinter.Button(\\1, text=\\2, command=\\3)\n" + \
+        "sg_button": "(?:\w+ ?= ?)(\w+).add_button\((.+), ?(\w+), ?(\d+)\d\)([ #\w]*)", 
+        "tk_button": "\\3_bt = Tkinter.Button(\\1, text=\\2, command=\\3)\\5\n" + \
                      "\\3_bt.config(width=\\4)\n" + \
                      "\\3_bt.grid()\n"}
         BUTTON_RE = re.compile(r"%s" % button_widget["sg_button"], re.MULTILINE)
