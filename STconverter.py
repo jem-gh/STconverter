fc8ed75a1e9702b0ebf59a51009991137d9a52ea
jem-gh <jem.github@gmail.com>
2012-11-06 18:15:45 +0100

    fix: sg>tk error when control_width specified

--- STconverter.py
+++ STconverter.py
@@ -68,7 +68,7 @@ class Simplegui2Tkinter:
         
         frame_widget = {
         "sg_frame":  "^(\w+) ?= ?simplegui.create_frame" + \
-                     "\((\".+\"), ?(\w+), ?(\w+)(?:, ?\d+)?\)", 
+                     "\((\".+\"), ?(\w+), ?(\w+),? *(\w+)?\)", 
         "tk_frame":  "window_root = Tkinter.Tk()\n" + \
                      "window_root.title(\\2)\n" + \
                      "\\1 = Tkinter.Frame(window_root)\n" + \
