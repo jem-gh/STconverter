29bc0f58ec97a24cd8152066bcc2d0c2193075d1
jem-gh <jem.github@gmail.com>
2012-11-06 14:14:33 +0100

    improve readability in converted code: return to line after setting up Canvas background

--- STconverter.py
+++ STconverter.py
@@ -77,7 +77,7 @@ class Simplegui2Tkinter:
         "tk_canvas": "w_canvas = Tkinter.Canvas(\\1, width=\\3, height=\\4)\n" + \
                      "w_canvas.grid()\n", 
         "sg_bg": "\w+.set_canvas_background\(\"(\w+)\"\)", 
-        "tk_bg": "w_canvas.configure(background='%s')", 
+        "tk_bg": "w_canvas.configure(background='%s')\n", 
         "sg_txt": "\w+.draw_text\((.+), ?" + \
                   "(\w+|[\[\(][\w ,]+[\]\)]), (\d+), (\"\w+\")\)", 
         "tk_txt": "w_canvas.create_text(\\2, anchor='sw', " + \
